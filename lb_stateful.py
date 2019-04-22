af7a7d57aa9e4e80420a54898bc01c57ed4ddd52
Fahmi Alfareza <fahmialfareza@icloud.com>
2019-04-22 08:01:45 +0700

    Skripsi

--- lb_stateful.py
+++ lb_stateful.py
@@ -121,8 +121,8 @@ class loadbalancer(app_manager.RyuApp):
                                 ip_proto=ip_header.proto, ipv4_src=ip_header.src, ipv4_dst=ip_header.dst,
                                 tcp_src=tcp_header.src_port, tcp_dst=tcp_header.dst_port)
 
-        server_mac_selected = "00:19:21:68:00:01"
-        server_ip_selected = "192.168.7.1"
+        server_mac_selected = ""
+        server_ip_selected = ""
         server_outport_selected = 1
 
         if tcp_header.dst_port == 80:
--- openflow13.py
+++ openflow13.py
@@ -101,15 +101,16 @@ class SimpleSwitch13(app_manager.RyuApp):
         actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
+        # if out_port != ofproto.OFPP_FLOOD:
+        #     match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+        #     # verify if we have a valid buffer_id, if yes avoid to send both
+        #     # flow_mod & packet_out
+        #     if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+        #         self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+        #         return
+        #     else:
+        #         self.add_flow(datapath, 1, match, actions)
+        
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
