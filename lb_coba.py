f9fa1770367727c4456a7afa9945662ab6dc2f6a
Fahmi Alfareza <fahmialfareza@icloud.com>
2019-06-26 17:22:50 +0700

    Skripsi

--- lb_coba.py
+++ lb_coba.py
@@ -206,7 +206,8 @@ class loadbalancer(app_manager.RyuApp):
 
             data2 = None
             if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-                data = msg.data
+                data2 = msg.data
 
-            out4 = parser.OFPPacketOut(datapath=datapath, in_port=server_outport_selected, actions=actions, data=data2)
+            out4 = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                      in_port=server_outport_selected, actions=actions, data=data2)
             datapath.send_msg(out4)
