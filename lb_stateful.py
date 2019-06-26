cc07ad24c18516eb41f9623eb8a2c46fcfb955f4
Fahmi Alfareza <fahmialfareza@icloud.com>
2019-06-26 08:29:09 +0700

    Skripsi

--- lb_stateful.py
+++ lb_stateful.py
@@ -129,9 +129,7 @@ class loadbalancer(app_manager.RyuApp):
             match = parser.OFPMatch(in_port=in_port, eth_type=eth.ethertype, eth_src=eth.src, eth_dst=eth.dst,
                                     ip_proto=ip_header.proto, ipv4_src=ip_header.src, ipv4_dst=ip_header.dst)
 
-            actions = [parser.OFPActionSetField(ipv4_src=self.virtual_lb_ip),
-                       parser.OFPActionSetField(eth_src=self.virtual_lb_mac),
-                       parser.OFPActionSetField(eth_dst=server_mac_selected),
+            actions = [parser.OFPActionSetField(eth_dst=server_mac_selected),
                        parser.OFPActionSetField(ipv4_dst=server_ip_selected),
                        parser.OFPActionOutput(server_outport_selected)]
             inst = [parser.OFPInstructionActions(
@@ -143,12 +141,10 @@ class loadbalancer(app_manager.RyuApp):
 
             # Reverse route from server
             match = parser.OFPMatch(in_port=server_outport_selected, eth_type=eth.ethertype, eth_src=server_mac_selected,
-                                    eth_dst=self.virtual_lb_mac, ip_proto=ip_header.proto, ipv4_src=server_ip_selected,
-                                    ipv4_dst=self.virtual_lb_ip)
+                                    eth_dst=eth.src, ip_proto=ip_header.proto, ipv4_src=server_ip_selected,
+                                    ipv4_dst=ip_header.src)
             actions = [parser.OFPActionSetField(eth_src=self.virtual_lb_mac),
                        parser.OFPActionSetField(ipv4_src=self.virtual_lb_ip),
-                       parser.OFPActionSetField(
-                           ipv4_dst=ip_header.src), parser.OFPActionSetField(eth_dst=eth.src),
                        parser.OFPActionOutput(in_port)]
             inst2 = [parser.OFPInstructionActions(
                 ofproto.OFPIT_APPLY_ACTIONS, actions)]
